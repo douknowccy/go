ce49c97ac21073a867b8de7dfa2d49399e4690c3
Falco <twkccy1990@gmail.com>
2023-09-11 19:32:22 +0800

    login and get token api

--- api/server.go
+++ api/server.go
@@ -2,6 +2,9 @@ package api
 
 import (
 	db "com.falco.go/db/sqlc"
+	"com.falco.go/token"
+	"com.falco.go/util"
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/gin-gonic/gin/binding"
 	"github.com/go-playground/validator/v10"
@@ -9,14 +12,18 @@ import (
 )
 
 type Server struct {
-	store  db.Store
-	router *gin.Engine
+	config     util.Config
+	store      db.Store
+	tokenMaker token.Maker
+	router     *gin.Engine
 }
 
-func NewServer(store db.Store) *Server {
-
-	server := &Server{store: store}
-	router := gin.Default()
+func NewServer(config util.Config, store db.Store) (*Server, error) {
+	tokenMaker, err := token.NewPasetoMaker(config.TokenSymmetricKey)
+	if err != nil {
+		return nil, fmt.Errorf("cannot create token maker %w", err)
+	}
+	server := &Server{store: store, tokenMaker: tokenMaker, config: config}
 
 	if v, ok := binding.Validator.Engine().(*validator.Validate); ok {
 		err := v.RegisterValidation("currency", validCurrency)
@@ -25,17 +32,21 @@ func NewServer(store db.Store) *Server {
 		}
 
 	}
+	server.setupRouter()
+	return server, nil
 
+}
+
+func (server *Server) setupRouter() {
+	router := gin.Default()
 	router.POST("/create_user", server.createUser)
-
+	router.POST("/login_user", server.loginUser)
 	router.POST("/create_account", server.createAccount)
 	router.GET("/account/:id", server.getAccount)
 	router.GET("/accounts", server.listAccount)
 
 	router.POST("/transfers", server.createTransfer)
 	server.router = router
-	return server
-
 }
 
 func (server *Server) Start(address string) error {
